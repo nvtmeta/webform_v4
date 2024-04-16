11eca30a73f1cd791806818918be735e767583ea
nguyen viet thinh <thinh30102002@gmail.com>
2024-04-16 09:01:11 +0700

    sidfinsd

--- ProductDetails.aspx.cs
+++ ProductDetails.aspx.cs
@@ -24,7 +24,8 @@ namespace WebApplication1
                     // Fetch product details from database using productId
                     // Replace the following code with your database access logic
                     string connectionString = "Server=localhost\\SQLEXPRESS;Database=db_ECommerceShop;Trusted_Connection=True;TrustServerCertificate=true;";
-                    string sql = "SELECT * FROM Products WHERE isDeleted = 0 and ProductId=@ProductId"; // Replace with your actual query
+                    string sql = "SELECT P.*, C.Name AS CategoryName FROM Products P INNER JOIN Categories C ON P.CategoryId = C.CategoryId" +
+                        " WHERE P.isDeleted = 0 and P.ProductId=@ProductId"; // Replace with your actual query
 
                     using (SqlConnection connection = new SqlConnection(connectionString))
                     {
@@ -40,18 +41,25 @@ namespace WebApplication1
                             {
                                 string imageUrl = reader["image_url"].ToString();
                                 string productName = reader["Name"].ToString();
+                                string category = reader["CategoryName"].ToString();
                                 string description = reader["Description"].ToString();
                                 string lastPrice = reader["LastPrice"].ToString();
                                 string price = reader["Price"].ToString();
 
                                 string productDetailHtml = $@"
 <section class=""text-gray-700 body-font overflow-hidden bg-white"">
-  <div class=""container px-5 py-24 mx-auto"">
-    <div class=""lg:w-4/5 mx-auto flex flex-wrap"">
-      <img alt=""ecommerce"" class=""lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"" src=""https://www.whitmorerarebooks.com/pictures/medium/2465.jpg"">
+  <div class=""container px-5 py-24 "">
+    <div class="" justify-start flex "">
+      <img alt=""ecommerce"" class="" w-fit object-cover object-center rounded-2xl 
+border border-gray-200"" src={imageUrl}>
       <div class=""lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0"">
-        <h2 class=""text-sm title-font text-gray-500 tracking-widest"">BRAND NAME</h2>
-        <h1 class=""text-gray-900 text-3xl title-font font-medium mb-1"">The Catcher in the Rye</h1>
+ <div class=""relative w-fit mb-3 grid select-none items-center whitespace-nowrap rounded-full border-[1px] border-solid border-blue-500
+py-1.5 px-3 font-sans text-xs font-bold uppercase text-blue-500"">
+      {category}
+    </div>
+        <h1 class=""text-gray-900 text-3xl title-font font-medium mb-1"">
+{productName}
+</h1>
         <div class=""flex mb-4"">
           <span class=""flex items-center"">
             <svg fill=""currentColor"" stroke=""currentColor"" stroke-linecap=""round"" stroke-linejoin=""round"" stroke-width=""2"" class=""w-4 h-4 text-red-500"" viewBox=""0 0 24 24"">
@@ -89,7 +97,7 @@ namespace WebApplication1
             </a>
           </span>
         </div>
-        <p class=""leading-relaxed"">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+        <p class=""leading-relaxed"">{description}</p>
         <div class=""flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5"">
           <div class=""flex"">
             <span class=""mr-3"">Color</span>
@@ -115,13 +123,15 @@ namespace WebApplication1
           </div>
         </div>
         <div class=""flex"">
-          <span class=""title-font font-medium text-2xl text-gray-900"">$58.00</span>
-          <button class=""flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"">Button</button>
-          <button class=""rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4"">
-            <svg fill=""currentColor"" stroke-linecap=""round"" stroke-linejoin=""round"" stroke-width=""2"" class=""w-5 h-5"" viewBox=""0 0 24 24"">
-              <path d=""M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z""></path>
-            </svg>
-          </button>
+          <span class=""title-font font-medium text-2xl text-red-500 mr-3"">${lastPrice} đ</span>
+          <a href=""#"" class=""flex p-2 items-center justify-center rounded-md bg-gradient-to-r
+            from-sky-400 to-blue-500 
+            px-5 text-center text-sm font-medium text-white hover:from-sky-500 hover:to-blue-600  
+                focus:outline-none focus:ring-4 focus:ring-blue-300"">
+                  <svg xmlns=""http://www.w3.org/2000/svg"" class=""mr-2 h-6 w-6"" fill=""none"" viewBox=""0 0 24 24"" stroke=""currentColor"" stroke-width=""2"">
+                    <path stroke-linecap=""round"" stroke-linejoin=""round"" d=""M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"" />
+                  </svg>
+                  Thêm vào giỏ </a
         </div>
       </div>
     </div>
